b1afcefdff8a9b587d935dbe81530cde9b4a8f28
ifadworld2 <48707822+ifadworld2@users.noreply.github.com>
2019-03-25 10:11:30 +0700

    a

--- js/main.js
+++ js/main.js
@@ -1,88 +1,133 @@
 /*
-* create database IndexedDB
+* IndexedDB
 * */
-
 createDatabase();
 function createDatabase() {
     if (!('indexedDB' in window)){
-        console.log('Web browser tidak mendukung IDB');
+        console.log('Web Browser tidak mendukung Indexed DB');
         return;
     }
-    var request = window.indexedDB.open('latihan-idb',1);
-    request.onerror = handleError;
-    request.onupgradeneeded = (e) => {
+    var request = window.indexedDB.open('latihan-pwa',1);
+    request.onerror = errordbHandle;
+    request.onupgradeneeded = (e)=>{
         var db = e.target.result;
-        db.onerror = handleError;
+        db.onerror = errordbHandle;
         var objectStore = db.createObjectStore('mahasiswa',
             {keyPath: 'nim'});
-        console.log('Objecct store mahasiswa berhasil dibuat');
+        console.log('Object store mahasiswa berhasil dibuat');
     }
     request.onsuccess = (e) => {
         db = e.target.result;
-        db.error = handleError;
-        console.log('Berhasil melakukan koneksi ke db lokal');
-        // do something...
-        
+        db.error = errordbHandle;
+        console.log('Berhasil melakukan koneksi ke database lokal');
+        // lakukan sesuatu ...
+        bacaDariDB();
     }
 }
 
-function handleError(e) {
+function errordbHandle(e) {
     console.log('Error DB : '+e.target.errorCode);
 }
 
-var nim = document.getElementById('nim'),
+var tabel = document.getElementById('tabel-mahasiswa'),
+    nim = document.getElementById('nim'),
     nama = document.getElementById('nama'),
     gender = document.getElementById('gender'),
-    form = document.getElementById('form-tambah'),
-    tabel = document.getElementById('tabel-mahasiswa');
+    form = document.getElementById('form-tambah');
 
-// tambah data
-form.addEventListener('submit', tambahBaris);
+form.addEventListener('submit',tambahBaris);
+tabel.addEventListener('click',hapusBaris);
 
-function tambahBaris(e) {
-    // cek apakah nim sudah ada
+function tambahBaris(e){
+    // cek nim apakah sudah ada
     if (tabel.rows.namedItem(nim.value)){
-        alert('Error : NIM sudah terdaftar');
+        alert('Error: NIM sudah terdaftar');
         e.preventDefault();
         return;
-    } 
-    
-    // tambahkan ke dalam database
-    tambahKeDatabase({
-        nim: nim.value,
-        nama: nama.value,
-        gender: gender.value
+    }
+    // masukkan data ke database
+    tambahKeDB({
+        nim : nim.value,
+        nama : nama.value,
+        gender : gender.value
     });
 
-    // nambahin baris
+    // append baris baru dari data form
     var baris = tabel.insertRow();
+    baris.id = nim.value;
     baris.insertCell().appendChild(document.createTextNode(nim.value));
     baris.insertCell().appendChild(document.createTextNode(nama.value));
     baris.insertCell().appendChild(document.createTextNode(gender.value));
 
-    // tambah tombol hapus
-    var tombolHapus = document.createElement('input');
-    tombolHapus.type = 'button';
-    tombolHapus.value = 'Hapus';
-    tombolHapus.id = nim.value;
-    baris.insertCell().appendChild(tombolHapus);
+    // tambah bagian button delete
+    var btn = document.createElement('input');
+    btn.type = 'button';
+    btn.value = 'Hapus';
+    btn.id = nim.value;
+    btn.className = 'btn btn-sm btn-danger';
+    baris.insertCell().appendChild(btn);
     e.preventDefault();
-
 }
 
-function tambahKeDatabase(mahasiswa) {
+function tambahKeDB(mahasiswa) {
     var objectStore = buatTransaksi().objectStore('mahasiswa');
     var request = objectStore.add(mahasiswa);
-    request.onerror = handleError;
-    request.onsuccess =
-        console.log('Mahasiswa ['+mahasiswa.nim+'] ditambahkan');
+    request.onerror = errordbHandle;
+    request.onsuccess = console.log('Mahasiswa ['+mahasiswa.nim+'] '
+        +'berhasil di tambahkan')
 }
-
 
 function buatTransaksi() {
     var transaction = db.transaction(['mahasiswa'],'readwrite');
-    transaction.onerror = handleError;
-    transaction.oncomplete = console.log('transaksi baru done');
+    transaction.onerror = errordbHandle;
+    transaction.complete = console.log('Transaksi selesai');
 
     return transaction;
+}
+
+function bacaDariDB() {
+    var objectStore = buatTransaksi().objectStore('mahasiswa');
+    objectStore.openCursor().onsuccess = (e) => {
+        var result = e.target.result;
+        if (result){
+            console.log('Membaca [' + result.value.nim +'] dari DB');
+            // append baris dari database
+            var baris = tabel.insertRow();
+            baris.id = nim.value;
+            baris.insertCell().appendChild(document.createTextNode(result.value.nim));
+            baris.insertCell().appendChild(document.createTextNode(result.value.nama));
+            baris.insertCell().appendChild(document.createTextNode(result.value.gender));
+
+            // append tombol hapus
+            var btn = document.createElement('input');
+            btn.type = 'button';
+            btn.value = 'Hapus';
+            btn.id = result.value.nim;
+            btn.className = 'btn btn-sm btn-danger';
+            baris.insertCell().appendChild(btn);
+            result.continue();
+        }
+    }
+}
+
+function hapusBaris(e) {
+    if (e.target.type ==='button'){
+        var hapus = confirm('Apakah yakin menghapus data?');
+        if (hapus){
+            tabel.deleteRow(tabel.rows.namedItem(e.target.id).sectionRowIndex);
+            hapusDariDB(e.target.id);
+        }
+    }
+}
+
+
+
+
+
+function hapusDariDB(nim) {
+    var objectStore = buatTransaksi().objectStore('mahasiswa');
+    var request = objectStore.delete(nim);
+    request.onerror = errordbHandle;
+    request.onsuccess = console.log('Mahasiswa ['+nim+'] terhapus');
+    
 }
